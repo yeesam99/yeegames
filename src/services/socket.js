d52b43c1e1b3592be06b12bce0fa6b2a226cf964
yeesam99 <eyo52624@gmail.com>
2026-05-03 22:15:10 +0900

    기능 추가

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,6 +1,9 @@
 import { io } from 'socket.io-client'
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:4000'
+// Vercel 환경변수 설정 필요:
+// VITE_SOCKET_URL=https://yeegames.onrender.com
+const LOCAL_SOCKET_URL = `http://localhost:${4000}`
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || LOCAL_SOCKET_URL
 
 console.log('[socket] connecting to:', SOCKET_URL)
 
@@ -8,3 +11,13 @@ export const socket = io(SOCKET_URL, {
   autoConnect: false,
   transports: ['websocket', 'polling'],
 })
+
+socket.on('connect', () => {
+  console.log('socket connected:', socket.id)
+})
+
+socket.on('connect_error', (err) => {
+  console.error('socket connection error:', err)
+})
+
+socket.connect()
