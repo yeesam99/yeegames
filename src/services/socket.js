011d4e7482ae84a4574aa240349d2c9d6566c4ae
yeesam99 <eyo52624@gmail.com>
2026-05-03 22:07:12 +0900

    기능 추가

--- src/services/socket.js
+++ src/services/socket.js
@@ -1,10 +1,8 @@
 import { io } from 'socket.io-client'
 
-const SOCKET_URL = import.meta.env.VITE_SOCKET_URL
+const SOCKET_URL = import.meta.env.VITE_SOCKET_URL || 'http://localhost:4000'
 
-if (!SOCKET_URL) {
-  console.warn('VITE_SOCKET_URL is not set')
-}
+console.log('[socket] connecting to:', SOCKET_URL)
 
 export const socket = io(SOCKET_URL, {
   autoConnect: false,
